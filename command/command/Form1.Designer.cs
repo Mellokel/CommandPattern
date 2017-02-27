aa88df2edf5720053b5a2e848fd46336eafc2dce
Mellokel <near.csd.me@gmail.com>
2017-02-27 23:31:49 +0700

    Command

---
+++ command/command/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace command
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Execute = new System.Windows.Forms.Button();
+            this.UnExecute = new System.Windows.Forms.Button();
+            this.First = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Count = new System.Windows.Forms.TextBox();
+            this.DrawDots = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Execute
+            // 
+            this.Execute.Location = new System.Drawing.Point(16, 215);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(131, 25);
+            this.Execute.TabIndex = 27;
+            this.Execute.Text = "Закрасить";
+            this.Execute.UseVisualStyleBackColor = true;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // UnExecute
+            // 
+            this.UnExecute.Location = new System.Drawing.Point(16, 246);
+            this.UnExecute.Name = "UnExecute";
+            this.UnExecute.Size = new System.Drawing.Size(131, 25);
+            this.UnExecute.TabIndex = 27;
+            this.UnExecute.Text = "Отменить действие";
+            this.UnExecute.UseVisualStyleBackColor = true;
+            this.UnExecute.Click += new System.EventHandler(this.UnExecute_Click);
+            // 
+            // First
+            // 
+            this.First.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.First.Location = new System.Drawing.Point(16, 176);
+            this.First.Multiline = true;
+            this.First.Name = "First";
+            this.First.Size = new System.Drawing.Size(78, 24);
+            this.First.TabIndex = 28;
+            this.First.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(218, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(209, 174);
+            this.panel1.TabIndex = 30;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 166);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Count
+            // 
+            this.Count.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Count.Location = new System.Drawing.Point(19, 58);
+            this.Count.Multiline = true;
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(78, 24);
+            this.Count.TabIndex = 28;
+            this.Count.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // DrawDots
+            // 
+            this.DrawDots.Location = new System.Drawing.Point(19, 88);
+            this.DrawDots.Name = "DrawDots";
+            this.DrawDots.Size = new System.Drawing.Size(128, 25);
+            this.DrawDots.TabIndex = 27;
+            this.DrawDots.Text = "Нарисовать вершины";
+            this.DrawDots.UseVisualStyleBackColor = true;
+            this.DrawDots.Click += new System.EventHandler(this.DrawDots_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Blue",
+            "Red",
+            "Yellow",
+            "Green"});
+            this.comboBox1.Location = new System.Drawing.Point(100, 176);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 31;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 558);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.First);
+            this.Controls.Add(this.UnExecute);
+            this.Controls.Add(this.DrawDots);
+            this.Controls.Add(this.Execute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Execute;
+        private System.Windows.Forms.Button UnExecute;
+        private System.Windows.Forms.TextBox First;
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox Count;
+        private System.Windows.Forms.Button DrawDots;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ command/command/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace command
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        float radius;
+        float widthX;
+        float heightY;
+        float x; float y;
+        Point pNode;
+        Image image1;
+        Graphics g;
+        Invoker Dots;
+
+        Color currentColor;
+        Color previousColor;
+        int numberOfLustElement;
+        int numberOfElement;
+
+
+        int nNodes;
+
+        private void DrawDots_Click(object sender, EventArgs e) 
+        {
+            Dots = new Invoker(this);
+            nNodes = Convert.ToInt32(Count.Text);
+            var command = new CreateCommand(this);
+            Dots.PressButton(command);
+        }
+
+        public void getNodePos(int k) // определяем позицию для квадрата (расположены по кругу)
+        {
+            double ugol = (2 * Math.PI) / (nNodes + 1) * k;
+            x = (float)(radius * Math.Cos(ugol) + widthX);
+            y = (float)(radius * Math.Sin(ugol) + heightY);
+           
+        }
+        
+        public void Draw() // рисуем квадраты на picturebox
+        {
+            image1 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            
+            
+            Pen pen = new Pen(Color.Red, 2);
+            g = Graphics.FromImage(image1);
+            widthX = pictureBox1.Width / 2;
+            heightY = pictureBox1.Height / 2;
+            radius = Math.Min(widthX, heightY) - 20;
+
+            for (int i = 0; i <= nNodes; i++)
+            {
+                pNode = new Point();
+                getNodePos(i);
+                pNode.X = Convert.ToInt32(x);
+                pNode.Y = Convert.ToInt32(y);
+                g.DrawRectangle(pen, pNode.X + 5, pNode.Y + 5, 10, 10);
+
+                g.FillRectangle(new SolidBrush(Color.Blue), pNode.X + 5, pNode.Y + 5, 10, 10);
+                g.DrawString(i.ToString(), new Font("new Times Roman", 10), Brushes.Black, pNode.X + 10, pNode.Y + 5);
+            }
+            pictureBox1.Image = image1;
+        }
+
+        public void Clear() { // очистка при отмене последней команды
+            pictureBox1.Image = null;
+        }
+        public void getNewParametrs() {
+            if (Convert.ToBoolean(numberOfElement))
+                numberOfLustElement = numberOfElement;
+            numberOfElement = Convert.ToInt32(First.Text);
+
+            getNodePos(numberOfElement);
+            pNode.X = Convert.ToInt32(x);
+            pNode.Y = Convert.ToInt32(y);
+
+            previousColor = ((Bitmap)pictureBox1.Image).GetPixel(Convert.ToInt32(x) + 5, Convert.ToInt32(y) + 5);
+            currentColor = Color.FromName(comboBox1.Text);
+        }
+
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                getNewParametrs();
+
+                Dots.PressButton(new ChangeColorCommand(this, previousColor, numberOfElement));
+            }
+        }
+
+        public void ChangeColor( )
+        {
+            g.FillRectangle(new SolidBrush(currentColor), pNode.X + 5, pNode.Y + 5, 10, 10);
+            pictureBox1.Image = image1;
+        }
+        public void ReChangeColor(Color color, int number) // отмена последней перерисовки квадрата
+        {
+            currentColor = color;
+            numberOfElement = number;
+
+            getNodePos(numberOfElement);
+            pNode.X = Convert.ToInt32(x);
+            pNode.Y = Convert.ToInt32(y);
+
+            g.FillRectangle(new SolidBrush(currentColor), pNode.X + 5, pNode.Y + 5, 10, 10);
+            pictureBox1.Image = image1;
+        }
+
+        private void UnExecute_Click(object sender, EventArgs e)
+        {
+            Dots.PressUndoButton();
+        }
+    }
+
+    class Reciever {
+        Form1 a;
+
+        public Reciever(Form1 a)
+        {
+
+
+        }
+
+
+    }
+
+
+    interface ICommand
+    {
+        string Name { get; }
+        void Execute();
+        void Undo();
+    }
+    
+    class CreateCommand : ICommand
+    {
+        Form1 a;
+
+        public string Name {
+            get { return "Create"; }
+        }
+
+        public CreateCommand(Form1 a)
+        {
+            this.a = a;
+
+        }
+
+        public void Execute()
+        {
+           a.Draw();
+        }
+        public void Undo()
+        {
+            a.Clear();
+        }
+    }
+
+    class ChangeColorCommand : ICommand
+    {
+        Form1 a;
+        Color color;
+        int number;
+
+        public string Name
+        {
+            get { return "Change color"; }
+        }
+
+        public ChangeColorCommand(Form1 a, Color color, int number) {
+            this.a = a;
+            this.color = color;
+            this.number = number;
+        }
+
+        public void Execute()
+        {
+            a.ChangeColor();
+        }
+
+        public void Undo()
+        {
+            a.ReChangeColor(color, number);
+        }
+    }
+
+    class Invoker
+    {
+        
+        Stack<ICommand> commandsHistory;
+
+        public Invoker(Form1 a)
+        {
+           commandsHistory = new Stack<ICommand>();
+        }
+
+        public void PressButton(ICommand command)
+        {
+            command.Execute();
+            commandsHistory.Push(command);
+        }
+        public void PressUndoButton()
+        {
+            if (commandsHistory.Count > 0)
+            {
+                var undoCommand = commandsHistory.Pop();
+                undoCommand.Undo();
+               
+            }
+        }
+    }
+    
+}
