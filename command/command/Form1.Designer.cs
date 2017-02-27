1f609646f55731a66efa9280760a3cec23269496
Mellokel <near.csd.me@gmail.com>
2017-02-27 23:42:00 +0700

    Command 1.1

--- command/command/Form1.Designer.cs
+++ command/command/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.Count = new System.Windows.Forms.TextBox();
             this.DrawDots = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -120,11 +121,21 @@
             this.comboBox1.Size = new System.Drawing.Size(100, 21);
             this.comboBox1.TabIndex = 31;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 13);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Укажите номер квадрата и цвет";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(471, 558);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.Count);
@@ -150,6 +161,7 @@
         private System.Windows.Forms.TextBox Count;
         private System.Windows.Forms.Button DrawDots;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
